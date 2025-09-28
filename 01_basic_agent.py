c3667442d4c486027c585a8fc5c2cc13792c949f
Surendranath Kumar <surendranath_kumar@gmail.com>
2025-09-28 00:28:17 -0700

    Initial commit

---
+++ 01_basic_agent.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+
+from langchain.agents import create_agent
+
+load_dotenv()
+
+def get_weather(city: str) -> str:
+    """
+    Get the weather for a given city.
+    """
+    return f"It's always Sunny in {city}!"
+
+agent = create_agent(
+    #model="openai:gpt-4o-mini", 
+    model=os.getenv("MODEL_NAME"),
+    tools=[get_weather],
+    prompt="You are a helpful assistant",
+)
+
+#Run agent
+response = agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
+
+print(response)
---
+++ instructions.md
@@ -0,0 +1,12 @@
+Step1:
+
+pip install --pre -U langchain
+
+Step2:
+
+# Installing the OpenAI integration
+pip install -U langchain-openai
+# Installing the Anthropic integration
+pip install -U langchain-anthropic
+
+
